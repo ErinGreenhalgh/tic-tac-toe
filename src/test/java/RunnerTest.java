a1a22a4dd96330f0a4856debddbc5cb74f024bb6
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-01-03 16:58:10 -0700

    create trivial Runner file with main method and first trivial test to ensure project is compiling correctly

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+public class RunnerTest {
+
+    @Test
+    public void firstTest(){
+        boolean truth = true;
+        assertEquals(truth, true);
+    }
+}
